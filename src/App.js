df49462c5ed601bd0dabf0c284634bf951d139b2
Sandipan Mondal <office.smondal@gmail.com>
2022-08-04 13:23:00 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(response => response.json())
+      .then(norris => this.setState({
+        value: norris.value
+      }))
+
+  }
+  refresh = () =>{
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(response => response.json())
+      .then(norris => this.setState({
+        value: norris.value
+      }))
+  }
+  render() {
+    return (
+      <div className='tc'>
+        <h1 className='f1'> Chuck Norris Jokes and Facts</h1>
+        <div className='tc bg-pink dib br3 pa3 ma2 grow bw2 shadow-5' style = {{background: "#f4f1de", color: "#e07a5f"}}>
+          <div className = "joke" >
+            
+              <h2>{this.state.value}</h2>   
+          </div>
+        </div>
+        <div>
+          <button onClick = {this.refresh} className = ' joke br3 pa2 ma2 grow bw1 shadow-3' type = "button" style = {{background: "#22333b", color: "white"}}> Refresh</button>
+        </div>
+      </div>
+      
+    );
+  }
+
 }
-
 export default App;
